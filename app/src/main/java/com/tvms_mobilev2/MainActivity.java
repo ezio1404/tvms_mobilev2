115f63b7458583e5faddb4edcbd31b05a78a1f97
EJ ANTON SABAL POTOT <zuming1404@gmail.com>
2019-01-27 18:03:18 +0800

    version 2 initial commit

---
+++ app/src/main/java/com/tvms_mobilev2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.tvms_mobilev2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btn_driverLog,btn_enforcerLog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_driverLog=findViewById(R.id.btn_driverLogin);
+        btn_enforcerLog=findViewById(R.id.btn_enforcerLogin);
+
+        btn_enforcerLog.setOnClickListener(this);
+        btn_driverLog.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_driverLogin){
+            startActivity(new Intent(this, driverLogin.class));
+        }
+        if(v.getId() == R.id.btn_driverLogin){
+            startActivity(new Intent(this, enforcerLogin.class));
+        }
+    }
+}
